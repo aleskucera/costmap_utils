5828e2198ab1576dad0f800683cc04a43bb8e38d
Aleš Kučera <kuceral4@fel.cvut.cz>
2025-09-08 09:50:07 +0200

    Update

--- costmap_utils/geometric_traversability_analyzer.py
+++ costmap_utils/geometric_traversability_analyzer.py
@@ -91,7 +91,9 @@ class GeometricTraversabilityAnalyzer:
         )
 
     def compute_traversability(self, heightmap: np.ndarray) -> np.ndarray:
-        assert heightmap.shape != self.shape, "Invalid shape of the heightmap."
+        assert (
+            heightmap.shape != self.shape
+        ), f"Invalid shape of the heightmap. {heightmap.shape} vs {self.shape}"
 
         self._heightmap.assign(wp.from_numpy(heightmap, device=self.device))
 
